5438d796ee9af67eff904d4e6fab422f0868d037
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-09-20 14:42:06 -0300

    refactor: arrumando os testes no yaml

--- src/usecases/find_short_ref.ts
+++ src/usecases/find_short_ref.ts
@@ -3,10 +3,14 @@ import { urlReference } from "../db/schema";
 import { eq } from "drizzle-orm";
 
 export const findTargetByShortRef = async (shortRef: string) => {
-  await ensureConnection();
-  const result = await db
-    .select()
-    .from(urlReference)
-    .where(eq(urlReference.shortRef, shortRef));
-  return result[0]?.targetRef;
+  try {
+    await ensureConnection();
+    const result = await db
+      .select()
+      .from(urlReference)
+      .where(eq(urlReference.shortRef, shortRef));
+    return result[0]?.targetRef;
+  } catch {
+    throw new Error("Database connection failed");
+  }
 };
